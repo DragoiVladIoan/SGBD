956c7d53e541a85a980b0a6765d90a5b26e46297
DragoiVladIoan <45821639+DragoiVladIoan@users.noreply.github.com>
2019-04-15 02:32:36 +0300

    Add project files.

---
+++ Lab1/FormChessDB.Designer.cs
@@ -0,0 +1,95 @@
+﻿namespace Lab1
+{
+    partial class FormChessDB
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.Save = new System.Windows.Forms.Button();
+            this.dataGridView2 = new System.Windows.Forms.DataGridView();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // Save
+            // 
+            this.Save.BackColor = System.Drawing.SystemColors.ControlLightLight;
+            this.Save.ForeColor = System.Drawing.Color.Black;
+            this.Save.Location = new System.Drawing.Point(557, 170);
+            this.Save.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.Save.Name = "Save";
+            this.Save.Size = new System.Drawing.Size(160, 86);
+            this.Save.TabIndex = 3;
+            this.Save.Text = "Save";
+            this.Save.UseVisualStyleBackColor = false;
+            this.Save.Click += new System.EventHandler(this.Save_Click);
+            // 
+            // dataGridView2
+            // 
+            this.dataGridView2.AccessibleName = "SpiesDataView";
+            this.dataGridView2.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView2.Location = new System.Drawing.Point(813, 11);
+            this.dataGridView2.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.dataGridView2.Name = "dataGridView2";
+            this.dataGridView2.Size = new System.Drawing.Size(467, 378);
+            this.dataGridView2.TabIndex = 0;
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.CausesValidation = false;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(43, 11);
+            this.dataGridView1.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(429, 378);
+            this.dataGridView1.TabIndex = 0;
+            // 
+            // FormChessDB
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ControlLightLight;
+            this.ClientSize = new System.Drawing.Size(1332, 519);
+            this.Controls.Add(this.dataGridView2);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.Save);
+            this.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.Name = "FormChessDB";
+            this.Text = "FormChessDB";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button Save;
+        public System.Windows.Forms.DataGridView dataGridView2;
+        public System.Windows.Forms.DataGridView dataGridView1;
+    }
+}
+
---
+++ Lab1/FormChessDB.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Lab1
+{
+    public partial class FormChessDB : Form
+    {
+        SqlConnection connection;
+        SqlDataAdapter adapterTournament; 
+        SqlDataAdapter adapterTournamentType; 
+        DataSet dataSet;
+        SqlCommandBuilder builder;
+        string connectionString = "Data Source = DESKTOP-SMI6GU7;" + "Initial Catalog = Chess Tournament;" + "Integrated Security = true";
+
+        public FormChessDB()
+        {
+            
+            connection = new SqlConnection(connectionString);
+            dataSet = new DataSet();
+            InitializeComponent();
+            LoadData();
+        }
+
+
+        private void Save_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                adapterTournament.SelectCommand.Connection = connection;
+                builder = new SqlCommandBuilder(adapterTournament);
+                adapterTournament.Update(dataSet, "Tournament");
+                dataSet.Tables["Tournament"].Clear();
+                adapterTournament.Fill(dataSet, "Tournament");
+
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void LoadData()
+        {
+            adapterTournament = new SqlDataAdapter("SELECT * FROM Tournament", connection);
+            adapterTournament.Fill(dataSet, "Tournament");
+
+            adapterTournamentType = new SqlDataAdapter("SELECT * FROM TournamentType", connection);
+            adapterTournamentType.Fill(dataSet, "TournamentType");
+
+            DataRelation relation = new DataRelation("FK__Tournament__TTID", dataSet.Tables["TournamentType"].Columns["TTID"], dataSet.Tables["Tournament"].Columns["TTID"]);
+            dataSet.Relations.Add(relation);
+
+            BindingSource bindingSourceParent = new BindingSource(dataSet, "TournamentType");
+            BindingSource bindingSourceChild = new BindingSource(bindingSourceParent, "FK__Tournament__TTID");
+
+            dataGridView1.DataSource = bindingSourceParent;
+            dataGridView2.DataSource = bindingSourceChild;
+        }
+
+       
+    }
+    
+}
